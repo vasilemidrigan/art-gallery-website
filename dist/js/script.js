71b1f22baef5ba67651029fbeb39ee1568525a08
Vasile Midrigan <111rigan111@gmail.com>
2022-05-08 12:25:13 +0100

    build header section for tablet version

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,16 @@
 "use strict";
 
 // Variables
+// Header
 const header = document.querySelector("header");
-const main = document.querySelector("main");
-const footer = document.querySelector("footer");
-const socialsFb = document.querySelector(".footer--socials--facebook");
+const headerImg = document.querySelector(".header--image");
 const buttonLink = document.querySelector(".header--button--link");
 const buttonTextContent = document.querySelector(".button--left_side");
-// --- MAIN SCRIPTS
+// Main
+const main = document.querySelector("main");
+// Footer
+const footer = document.querySelector("footer");
+const socialsFb = document.querySelector(".footer--socials--facebook");
 
 // Change main styles if the location page is opened:
 if (location.pathname === "/location.html") {
@@ -17,4 +20,9 @@ if (location.pathname === "/location.html") {
   buttonLink.href = "./index.html";
   buttonTextContent.innerHTML = "Back to home";
 }
-console.log(buttonLink);
+
+// Change Styles (for Tablet Screens):
+// window.addEventListener("resize", () => {
+if (window.innerWidth > 770 && window.innerWidth < 1400) {
+  headerImg.src = "./starter-code/assets/tablet/image-hero@2x.jpg";
+}
